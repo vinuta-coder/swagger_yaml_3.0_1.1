db53234a1112df863af1736c39cb5ed33dfb401f
@AshishTom_theGeekHater <32588411+ASHISHTOM@users.noreply.github.com>
2020-03-30 21:05:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-/**
- * Created by Bikram Pandit on 8/18/2018.
- */
 function submit() {
     let s = document.getElementById("json_data").value;
-    document.getElementById('yaml_out').value = '';
-    let json_object = JSON.parse(s);
-    parse(json_object, '')
+    let path = document.getElementById("path").value;
+    let tag = document.getElementById("tag").value;
+    let model = document.getElementById("model").value;
+    let type = document.getElementById("type").value;
+
+    if (!validateForm(s, path, tag, model, type)) {
+        document.getElementById('yaml_out').value = 'Please Fill All Required Field';
+
+    } else {
+        try {
+            document.getElementById('yaml_out').value = '';
+            let json_object = JSON.parse(s);
+            writeHead(path, tag, model, type);
+            parse(json_object, '    ');
+        } catch (e) {
+            document.getElementById('yaml_out').value = e;
+        }
+    }
 }
 
 function isEmpty(obj) {
@@ -18,32 +30,85 @@ function isEmpty(obj) {
 }
 
 function parse(s, indent) {
+
     if (Array.isArray(s)) {
-        write(indent + 'type: array');
+        write(indent + 'type: \"array\"');
         write(indent + 'items:');
         if (s.length !== 0) {
             parse(s[0], indent + '  ');
         } else {
-            write(indent + '  type: object');
+            write(indent + '  type: \"object\"');
         }
     } else if (typeof s === 'object') {
-        write(indent + 'type: object');
+        write(indent + 'type: \"object\"');
         if (isEmpty(s)) {
             return;
         }
         write(indent + 'properties:');
         for (let key in s) {
             write(indent + '  ' + key + ':');
-            if(s.hasOwnProperty(key)){
+            if (s.hasOwnProperty(key)) {
                 parse(s[key], indent + '    ');
             }
         }
     } else {
-        write(indent + 'type: ' + (typeof s));
+        write(indent + 'type: \"' + (typeof s) + '\"');
     }
 }
 
 function write(s) {
     // console.log(s);
     document.getElementById('yaml_out').value += s + "\n";
+}
+
+function writeHead(path, tag, model, type) {
+
+
+    var header = `swagger: '3.0.0'
+info:
+  version: 1.0.0
+  title: Swagger Api
+
+tags:
+  - name: `+ tag + `
+    description: Everything about `+ tag + `
+    externalDocs:
+      description: Find out more
+      url: 'http://10.0.0.69:9999/swagger/'
+servers:
+  - url: http://10.0.0.69:8080/OM
+paths:
+  /`+ path + `:
+    `+ type + `:
+      tags:
+        - `+ tag + `
+      summary: Add a operation
+      requestBody:
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/`+ model + `'
+      responses:
+          '200':
+            description: successful operation
+            content:
+              application/json:
+                schema:
+                  $ref: '#/components/schemas/`+ model + `_response'
+          '405':
+            description: Invalid input
+components:
+  schemas:
+    `+ model + `:`;
+
+      write(header);
+
+}
+
+function validateForm(json, path, tag, model, type) {
+    if (json == null || json == "" || path == null || path == "" || tag == null || tag == "" || model == null || model == "" || type == null || type == "") {
+
+        return false;
+    }
+    return true;
 }
