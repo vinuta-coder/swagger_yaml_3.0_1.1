8957d7ee2152f3c4535402adb21e5ccb889f0499
@AshishTom_theGeekHater <32588411+ASHISHTOM@users.noreply.github.com>
2020-04-10 15:41:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function parse(s, indent) {
         //write(indent + '  '+'-  ""');
         write(indent + 'required:');  
 	for (let key in s) {
-            var regex = new RegExp("0");
+            var regex = new RegExp(" ");
             if (regex.test(key)){
               write(indent + '  ' + '-  "'+ key + '"');
             }
